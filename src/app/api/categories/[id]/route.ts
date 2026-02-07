409f7a510dbc0562c4204a6a69e9ec5f94a8068e
Harshil-Shah-code <harshilshah1004@gmail.com>
2026-02-07 15:40:14 +0530

    feat: add API route for deleting a category by ID

--- src/app/api/categories/[id]/route.ts
+++ src/app/api/categories/[id]/route.ts
@@ -4,8 +4,9 @@ import Category from '@/models/Category';
 
 export async function DELETE(
     request: Request,
-    { params }: { params: { id: string } }
+    props: { params: Promise<{ id: string }> }
 ) {
+    const params = await props.params;
     try {
         await dbConnect();
         const { id } = params;
